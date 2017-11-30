1f25945adf1d14dcc315fd75549de34c5cb0d0de
whisi <2015whisi@gmail.com>
2017-11-30 16:33:23 +0900

    first

---
+++ db/seeds.rb
@@ -0,0 +1,10 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+Post.create(uploader: "완수", text: "완수입니다", likes: 1000, url: "http://cfile24.uf.tistory.com/image/15733D454EFE68C1026207")
+Post.create(uploader: "윤미", text: "윤미입니다", likes: 3, url: "http://cfile24.uf.tistory.com/image/15733D454EFE68C1026207")
+Post.create(uploader: "선경", text: "선경입니다", likes: 5, url: "http://cfile24.uf.tistory.com/image/15733D454EFE68C1026207")
